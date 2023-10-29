5759c76ba737de9bfacfae36b6befd6aa4a87ee7
yaroslav <yaosobolev@gmail.com>
2023-10-29 22:21:19 +0500

    ddc

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,14 +4,6 @@ import Root from "../pages/RootLayout.tsx";
 
 import HomePage from "../pages/landing/home/page.tsx";
 
-import AuthLayout from "../pages/auth/AuthLayout.tsx";
-import SignInPage from "../pages/auth/sign-in/SignInPage.tsx";
-import SignUpPage from "../pages/auth/sign-up/SignUpPage.tsx";
-
-import ConfirmEmailPage, {
-  loader as confirmEmailLoader,
-} from "../pages/auth/confirm-email/ConfirmEmailPage.tsx";
-
 const router = createBrowserRouter([
   {
     path: "/",
@@ -21,25 +13,7 @@ const router = createBrowserRouter([
         path: "/",
         element: <HomePage />,
       },
-      {
-        element: <AuthLayout />,
-        children: [
-          {
-            path: "/sign-in",
-            element: <SignInPage />,
-          },
-          {
-            path: "/sign-up",
-            element: <SignUpPage />,
-          },
-          {
-            path: "/confirm-email",
-            element: <ConfirmEmailPage />,
-            loader: confirmEmailLoader,
-          },
-        ],
-      },
-      // {
+
       //   path: "/dashboard/mytest",
       //   element: <DashboardPage />,
       // },
