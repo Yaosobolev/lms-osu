b5ff5c2e0bc140d795cabb4a9a0e0cf25d022fae
yaroslav <yaosobolev@gmail.com>
2023-10-29 21:58:35 +0500

    zxc

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Root from "../pages/RootLayout.tsx";
-import DashboardLayout from "../pages/dashboard/DashboardLayout.tsx";
-import UserPage from "../pages/dashboard/user/UserPage.tsx";
+
 import HomePage from "../pages/landing/home/page.tsx";
 
 import AuthLayout from "../pages/auth/AuthLayout.tsx";
 import SignInPage from "../pages/auth/sign-in/SignInPage.tsx";
 import SignUpPage from "../pages/auth/sign-up/SignUpPage.tsx";
-
-import DashboardPage from "../pages/mytest/page.tsx";
-import LayoutTesty from "../pages/mytest/layout.tsx";
-import TestPage from "../pages/mytest/testy/index.tsx";
 
 import ConfirmEmailPage, {
   loader as confirmEmailLoader,
@@ -45,56 +40,9 @@ const router = createBrowserRouter([
         ],
       },
       // {
-      //   path: "dashboard",
-      //   children: [],
-      // },
-      {
-        path: "/dashboard",
-        element: <DashboardLayout></DashboardLayout>,
-        children: [
-          {
-            path: "user",
-            element: <UserPage />,
-            children: [],
-          },
-          {
-            path: "mytest",
-            element: <LayoutTesty></LayoutTesty>,
-            children: [
-              {
-                path: "",
-                element: <DashboardPage />,
-                children: [],
-              },
-              {
-                path: "test",
-                element: <TestPage />,
-                children: [],
-              },
-            ],
-          },
-        ],
-      },
-      // {
       //   path: "/dashboard/mytest",
       //   element: <DashboardPage />,
       // },
-      {
-        path: "/mytest",
-        element: <LayoutTesty></LayoutTesty>,
-        children: [
-          {
-            path: "user",
-            element: <DashboardPage />,
-            children: [],
-          },
-          {
-            path: "test",
-            element: <TestPage />,
-            children: [],
-          },
-        ],
-      },
     ],
   },
 ]);
